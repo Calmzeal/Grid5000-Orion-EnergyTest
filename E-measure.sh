7f3453b1c94fdb3e0ee405c5e18311dcc4c24bd6
Zeal <enlin3hao@163.com>
2018-12-07 15:14:17 +0100

    Create E-measure.sh

---
+++ E-measure.sh
@@ -0,0 +1,12 @@
+timemeasureC(){
+    date +%s >> $2
+    for I in `seq 1 $1`
+    do
+        echo $I >> $3
+        /root/chameleon/build/timing/time_dgemm_tile_batch --n=$(($4*$5)) --m=$(($4*$5)) --nb=$NB --gpus=0 --nowarmup >> $3
+        date +%s >> $2
+    done
+}
+
+export STARPU_SCHED=ws
+timemeasureC 5 CN1600S20.txt 20 1600
