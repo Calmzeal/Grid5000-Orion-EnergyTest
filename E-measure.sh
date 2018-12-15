6feb6b57eb0ec34cbacd711aa51118dcea72011d
Zeal <enlin3hao@163.com>
2018-12-15 18:52:44 +0100

    Update E-measure.sh

--- E-measure.sh
+++ E-measure.sh
@@ -1,13 +1,17 @@
 timemeasureC(){
-    date +%s >> $2
+    # date +%s >> $2
     for I in `seq 1 $1`
     do
-        echo $I >> $3
-        /root/chameleon/build/timing/time_dgemm_tile_batch --n=$(($4*$5)) --m=$(($4*$5)) --nb=$5 --gpus=0 --nowarmup --profile >> $3
-        date +%s >> $2
+        #echo $I >> $3
+        DURING=$(/root/chameleon/build/timing/time_dgemm_tile_batch --n=$(($3*$4)) --m=$(($3*$4)) --nb=$4 --gpus=0 --nowarmup --profile | grep "+-" | awk '{print $4}')  
+        FINISH=$(date +%s)
+        echo $(($FINISH-$DURING)) >> $2
+        echo $FINISH >> $2
     done
+    
+    
 }
 
 export STARPU_SCHED=ws
-timemeasureC 5 CN1600S20.txt logCN1600S20.txt 20 1600
+timemeasureC 5 CN1600S20.txt 20 1600
 
