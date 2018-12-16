5555f7642cfaaf2c5e7e5a734d6478cf53aefa85
Zeal <enlin3hao@163.com>
2018-12-16 11:27:30 +0100

    Update getWatt.sh

--- getWatt.sh
+++ getWatt.sh
@@ -1,16 +1,13 @@
 measure (){
-    curl "http://kwapi.lyon.grid5000.fr:12000/power/timeseries/?from=$1&to=$2&only=orion-1" >>$3
+    curl "http://kwapi.lyon.grid5000.fr:12000/power/timeseries/?from=$1&to=$2&only=orion-1" |jq '.["items"][0]["values"]' >>$3
 }
-
-
-
 teststarPU (){
-    read START<$1
-    while read f
+    while read s
     do
-        echo $START $f
-        measure $START $f $2
-        START=$f
+        read f<$1
+        echo $s $f
+        measure $s $f $2
     done<$1
 }
-teststarPU record1206GPU.txt test1206GPU.txt
+teststarPU CN1600S20.txt logCN1600S20.json
+
