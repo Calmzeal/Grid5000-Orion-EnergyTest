6de5ffe6215bc01b63fe6a178d5b04edf3ad2d7d
Zeal <enlin3hao@163.com>
2018-12-16 12:42:42 +0100

    Update E-measure.sh

--- E-measure.sh
+++ E-measure.sh
@@ -5,8 +5,7 @@ timemeasureC(){
         #echo $I >> $3
         DURING=$(/root/chameleon/build/timing/time_dgemm_tile_batch --n=$(($3*$4)) --m=$(($3*$4)) --nb=$4 --gpus=0 --nowarmup --profile | grep "+-" | awk '{print int($4)}')  
         FINISH=$(date +%s)
-        echo $(($FINISH-$DURING)) >> $2
-        echo $FINISH >> $2
+        echo $(($FINISH-$DURING)) " " $FINISH >> $2
     done
     echo "teststarPU $2 log$2" >> getWatt.sh
     scp $2 ezhu@lyon:~/
