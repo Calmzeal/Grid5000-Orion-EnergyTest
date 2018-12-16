4005b42054059bb3883080faa093c8e41307aa91
Zeal <enlin3hao@163.com>
2018-12-16 11:32:26 +0100

    Update E-measure.sh

--- E-measure.sh
+++ E-measure.sh
@@ -3,7 +3,7 @@ timemeasureC(){
     for I in `seq 1 $1`
     do
         #echo $I >> $3
-        DURING=$(/root/chameleon/build/timing/time_dgemm_tile_batch --n=$(($3*$4)) --m=$(($3*$4)) --nb=$4 --gpus=0 --nowarmup --profile | grep "+-" | awk '{print $4}')  
+        DURING=$(/root/chameleon/build/timing/time_dgemm_tile_batch --n=$(($3*$4)) --m=$(($3*$4)) --nb=$4 --gpus=0 --nowarmup --profile | grep "+-" | awk '{print int($4)}')  
         FINISH=$(date +%s)
         echo $(($FINISH-$DURING)) >> $2
         echo $FINISH >> $2
