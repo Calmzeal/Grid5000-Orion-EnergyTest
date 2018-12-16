128ce2896cda86809e693c0dd7088f716c420114
Zeal <enlin3hao@163.com>
2018-12-16 12:44:03 +0100

    Update getWatt.sh

--- getWatt.sh
+++ getWatt.sh
@@ -2,10 +2,10 @@ measure (){
     curl "http://kwapi.lyon.grid5000.fr:12000/power/timeseries/?from=$1&to=$2&only=orion-1" |jq '.["items"][0]["values"]' >>$3
 }
 teststarPU (){
-    while read s
+    while read line
     do
-        read f<$1
-        echo $s $f
+        s=$(echo $line | awk {print $1})
+        f=$(echo $line | awk {print $2})
         measure $s $f $2
     done<$1
 }
