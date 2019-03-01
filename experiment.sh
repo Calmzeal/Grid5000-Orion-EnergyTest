b45738589930681819e1cbada09473e1e03d757e
Zeal <enlin3hao@163.com>
2019-03-01 15:19:23 +0100

    update experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -73,6 +73,20 @@ tm-dgemm-GPU(){
     echo "teststarPU $timeout log$timeout" >> getWatt.sh
 }
 
+tm-cholesky(){
+    output="cholesky_N=$3-blocksize=$4_GPU-DeviceN=$2.txt"
+    timeout="cholesky_N=$3-blocksize=$4_GPU-DeviceN=$2_time.txt"
+    for I in `seq 1 $1`
+    do
+        /root/chameleon/build/timing/time_dpotrf_tile --n=$3 --m=$3 --nb=$4 --gpus=1 --threads=$2 --nowarmup --profile > temp 2>&1  
+        DURING=$(cat temp|grep "CUDA.*total"|awk {'print int($12)'})  
+        FINISH=$(date +%s)
+        cat temp >> $output
+        echo $(($FINISH-$DURING)) " " $FINISH >> $timeout
+    done
+    echo "teststarPU $timeout log$timeout" >> getWatt.sh
+}
+
 tm-dtrsm-GPU(){
     output="dtrsm_N=$3-blocksize=$4_GPU-DeviceN=$2.txt"
     timeout="dtrsm_N=$3-blocksize=$4_GPU-DeviceN=$2_time.txt"
@@ -112,10 +126,10 @@ cd /root/chameleon/build/timing
 make
 cd /root/HPC-research
 
-tm-dgemm-CPU 3 6 28800 200
-tm-dpotrf-CPU 3 6 28800 200
-tm-dtrsm-CPU 3 6 28800 200
-tm-dsyrk-CPU 3 6 28800 200
+# tm-dgemm-CPU 3 6 28800 200
+# tm-dpotrf-CPU 3 6 28800 200
+# tm-dtrsm-CPU 3 6 28800 200
+# tm-dsyrk-CPU 3 6 28800 200
 
 cp -f GPUversion/time_zpotrf_tile.c /root/chameleon/timing
 cp -f GPUversion/time_zgemm_tile_batch.c /root/chameleon/timing
@@ -123,14 +137,15 @@ cp -f GPUversion/time_zgemm_tile_batch.c /root/chameleon/timing
 cd /root/chameleon/build/timing
 make
 cd /root/HPC-research
+tm-cholesky 3 1 28800 200
+tm-cholesky 3 6 28800 200
+tm-cholesky 3 12 28800 200
 
-tm-dgemm-GPU 3 0 28800 200
-tm-dpotrf-GPU 3 0 28800 200
-tm-dtrsm-GPU 3 0 28800 200
-tm-dsyrk-GPU 3 0 28800 200
+# tm-dgemm-GPU 3 0 28800 200
+# tm-dtrsm-GPU 3 0 28800 200
+# tm-dsyrk-GPU 3 0 28800 200
 
-tm-dgemm-GPU 3 6 28800 200
-tm-dpotrf-GPU 3 6 28800 200
-tm-dtrsm-GPU 3 6 28800 200
-tm-dsyrk-GPU 3 6 28800 200
+# tm-dgemm-GPU 3 6 28800 200
+# tm-dtrsm-GPU 3 6 28800 200
+# tm-dsyrk-GPU 3 6 28800 200
 
